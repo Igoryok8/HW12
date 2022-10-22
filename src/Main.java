935eb66489ce5bdfa3ffb442fd479a7330d0140d
Igoryok8 <energymag@mail.ru>
2022-10-23 02:03:12 +0300

    Задание 1 выполнено

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,18 @@ public class Main {
 
         System.out.println("Залание 1");
         System.out.println();
+        checkLeapYear();
 
+    }
+    public static void checkLeapYear () {
+        int year= 2000;
+        int leapYear = year % 4;
+        if (leapYear==0 && year%400==0) {
+            System.out.println(year + " — високосный год");
+        } else {
+            System.out.println(year + " — не високосный год");
+
+        }
     }
 
 }
